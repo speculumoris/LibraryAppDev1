133bbc5fa193b9cdb2b7259fc20973e65acb7927
speculumoris <sefa.byndr4@gmail.com>
2023-03-05 22:21:54 +0300

    ImageFile

--- src/main/java/com/lib/dto/BookDTO.java
+++ src/main/java/com/lib/dto/BookDTO.java
@@ -1,11 +1,17 @@
 package com.lib.dto;
 
+import com.lib.domain.ImageFile;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,4 +19,31 @@ import org.springframework.stereotype.Service;
 @NoArgsConstructor
 public class BookDTO {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String isbn;
+
+    private Integer pageCount;
+
+    private Long authorId;
+    private Long publisherId;
+    private Integer publishDate;
+    private Long categoryId;
+
+    private ImageFile imageFile;
+
+
+    private boolean loanable;
+
+    private String shelfCode;
+
+    private boolean active;
+    private boolean featured;
+
+    private LocalDateTime createDate;
+    private boolean builtIn;
 }
