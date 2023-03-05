65ad7556bec0a5cf138d0982d7e39eaf655a9156
Mustafa Keser <keserzade@gmail.com>
2023-03-05 22:28:54 +0300

    denem2

--- src/main/java/com/lib/repository/BookRepository.java
+++ src/main/java/com/lib/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.lib.repository;
 
+import com.lib.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 
 }
