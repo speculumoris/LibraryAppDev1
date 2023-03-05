aa28358287998a48852b33026e5b8d919fce7c42
speculumoris <sefa.byndr4@gmail.com>
2023-03-05 22:14:41 +0300

    DTO

---
+++ src/main/java/com/lib/dto/BookDTO.java
@@ -0,0 +1,16 @@
+package com.lib.dto;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDTO {
+
+}
