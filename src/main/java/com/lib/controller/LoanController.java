e02ff40a582da67072b96d87443baed35d2049e7
speculumoris <sefa.byndr4@gmail.com>
2023-03-08 00:07:33 +0300

    Loan

---
+++ src/main/java/com/lib/controller/LoanController.java
@@ -0,0 +1,11 @@
+package com.lib.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loan")
+public class LoanController {
+
+
+}
