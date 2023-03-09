9565c491f9f7f6d5b15a5ca81828128fd9eaa2f8
Beyza KESER <beyzaaakeser@gmail.com>
2023-03-09 15:52:14 +0300

    author11

--- src/main/java/com/lib/LibraryAppApplication.java
+++ src/main/java/com/lib/LibraryAppApplication.java
@@ -3,7 +3,7 @@ package com.lib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.lib.service","com.lib.mapper","com.lib.security"})
+@SpringBootApplication
 public class LibraryAppApplication {
 
     public static void main(String[] args) {
